75e8c280b0c86d70c7a6f028541ca89ff9f64d1e
Fernando Acosta <fernandodeveloper99@gmail.com>
2022-05-30 23:05:46 -0500

    Python Administrator

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#MEDIA FILES
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR, 'media').resolve()
